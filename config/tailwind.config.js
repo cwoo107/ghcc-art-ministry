b8bfa7e3a19f1048874a54b3f9e328458648e921
cwoo107 <woodscaleb@gmail.com>
2023-08-10 09:13:16 -0700

    fixed background images

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'my-image-title': "url('/assets/background.webp')",
+        'my-image': "url('/assets/background_no_title.jpg')"
+      },
       colors: {
         brown: {
           50: '#fdf8f6',
@@ -21,6 +25,18 @@ module.exports = {
           700: '#977669',
           800: '#846358',
           900: '#45321c',
+        },
+        cream: {
+          50: '#fcfaf6',
+          100: '#faf6ee',
+          200: '#f7f1e6',
+          300: '#f5edde',
+          400: '#f2e8d6',
+          500: '#f0e4ce',
+          600: '#eddfc6',
+          700: '#ebdbbe',
+          800: '#e8d6b6',
+          900: '#e6d2ae',
         }},
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
